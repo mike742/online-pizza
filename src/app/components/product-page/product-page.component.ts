43dd3271eecf9877c802de12a65f2e190775892f
Mike Zorin <mike.zorin@robertsoncollege.ca>
2021-09-10 10:46:37 -0500

    added Routing

--- src/app/components/product-page/product-page.component.ts
+++ src/app/components/product-page/product-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+  styleUrls: ['./product-page.component.css'],
 })
 export class ProductPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    console.log(this.route.snapshot.paramMap.get('id'));
   }
-
 }
